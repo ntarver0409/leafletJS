6de1a1d927e9a3a031102c520750ff6530645190
ntarver0409 <nctarver4994@icloud.com>
2021-03-11 22:31:25 -0600

    Finally got circles to plot, added legend

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,45 +1,9 @@
 // Constructing my url to pull the JSON
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 //Making a function to fill my cricles according to quake depth
-function getColor(d) {
-  return d > 100 ?'#D53E4F' :
-        d > 80  ?'#FC8D59' :
-        d > 60  ?'#FEE08B' :
-        d > 40  ?'#E6F598' :
-        d > 20   ?'#99D594' :
-                  '#3288BD';
-};
 
-//Pulling in the JSON from the USGS site
-d3.json(queryUrl, function(data) {
-  //Putting my response into the create bubbles feature.
-  createFeatures(data.features);
-  console.log(data.features);
-});
 
-function createFeatures(quakes) {
-  //Creating a function to make the circles for each feature
-  function onEachFeature (feature, layer) {
-    L.circle(
-      [feature.geometry.coordinates[1], feature.geometry.coordinates[0]], {
-        fillOpacity: 0.75,
-        color: "white",
-        fillColor: getColor(feature.geometry.coordinates[2]),
-        radius: feature.properties.mag * 3
-      }).bindPopup("<h3>" + feature.properties.title +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-  }
-  let earthquakes = L.geoJSON(quakes, {
-    onEachFeature: onEachFeature
-  });
-
-  createMap(earthquakes);
-};
-
-function createMap(earthquakes) {
-
-  //making my base lightmap
-  let lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+let lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
@@ -48,33 +12,49 @@ function createMap(earthquakes) {
     accessToken: API_KEY
   });
 
-  let baseMaps = {
-    "Light Map": lightmap
-  }
-
-  let overlayMaps = {
-    Earthquakes: earthquakes
-  }
-
   let myMap = L.map("map", {
     center: [
       37.09, -95.71
     ],
     zoom: 5,
-    layers: [lightmap, earthquakes]
+    layers: [lightmap]
   });
 
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
+//Pulling in the JSON from the USGS site
+d3.json(queryUrl, function(data) {
+  function styleInfo(feature){
+    return {fillOpacity: 0.75,
+            color: "white",
+            weight: 1,
+            fillColor: getColor(feature.geometry.coordinates[2]),
+            radius: feature.properties.mag * 4};
+  };
+  function getColor(d) {
+    return d > 100 ?'#D53E4F' :
+          d > 80  ?'#FC8D59' :
+          d > 60  ?'#FEE08B' :
+          d > 40  ?'#E6F598' :
+          d > 20   ?'#99D594' :
+                    '#3288BD';}
+
+  L.geoJSON(data, {
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng);},
+    style: styleInfo, 
+    onEachFeature: function onEachFeature (feature, layer) {
+      layer.bindPopup("<h3>" + feature.properties.title +
+        "</h3><hr><p>" + new Date(feature.properties.time) + "</p><hr><p>" + "Depth: "
+        + feature.geometry.coordinates[2] + " km" + "</p>");
+    }
   }).addTo(myMap);
 
   let legend = L.control({position: 'bottomright'});
 
-  legend.onAdd = function (myMap) {
+  legend.onAdd = function() {
 
-    var div = L.DomUtil.create('div', 'info legend'),
-        depths = [0, 20, 40, 60, 80, 100],
-        labels = [];
+    const div = L.DomUtil.create('div', 'info legend');
+    const depths = [0, 20, 40, 60, 80, 100];
+    let labels = [];
 
     // loop through our density intervals and generate a label with a colored square for each interval
     for (var i = 0; i < depths.length; i++) {
@@ -87,4 +67,8 @@ function createMap(earthquakes) {
 };
 
   legend.addTo(myMap);
-};
+});
+
+
+
+
